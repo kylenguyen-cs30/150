b616442465e2b23bd6a2e767e2242be07c6466eb
Kyle Nguyen <knguyen1193@outlook.com>
2023-12-07 16:41:29 -0800

    still fixing

---
+++ solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int strStr(string haystack, string needle) {
+    
+
+}
+
+int main(int argc, char const *argv[])
+{
+    string haystack = "sadbutsad";
+    string needle = "sad";
+
+    return 0;
+}
