7a1fb16e19464ca0c97e2036093bf17288f75a7b
Kyle <knguyen1193@outlook.com>
2023-12-07 21:22:13 -0800

    test case input 'leeto'

--- solution.cpp
+++ solution.cpp
@@ -5,14 +5,81 @@ using namespace std;
 
 
 int strStr(string haystack, string needle) {
-    
+	int n = haystack.length();
+	int m = needle.length();
+	int found_index = 0;
+	int count = 0;
+	int index1 = 0;
+	int index2 = 0;
+	while(index1 < n){
+		
+		//first case
+		if(needle[index2] == haystack[index1]){
+			count++;
+			index2++;
+		}else if(needle[index2] != haystack[index1]){
+			count = 0;
+			index2 = 0;
+		}else if(count == m){
+			found_index = index1 - m;
+			break;
+		}
+		index1++;
+	}
+
+	return found_index;	
 
 }
 
 int main(int argc, char const *argv[])
 {
-    string haystack = "sadbutsad";
-    string needle = "sad";
+    string haystack = "leetcode";
+    string needle = "leeto";
+
+    int result = strStr(haystack, needle);
+
+    cout << result << endl;
 
     return 0;
 }
+
+/* - two string needle, haystack
+ * - return the index of the first occurrence of needle in haystack
+ * - return -1 if there is no needle
+ * 
+ *
+ * pseudocode:
+ *
+ * string haystack
+ * string needle
+ * 
+ * int n = haystack.length()
+ * int m = needle.length()
+ * 
+ * bool found = false
+ *
+ * int found_index = 0	
+ *
+ * int count = 0
+ * int index1 = 0
+ * int index2 = 0
+ * while(index1 < n){
+ *	
+ *	if needle[index2] == haystack[index1]:
+ *		count++
+ *		index2++
+ *		
+ *	else if needle[index2] != haystack[index1]:
+ *		count = 0
+ *		index2 = 0
+ *	
+ *	else if count == m:
+ *		found_index = index1 - m
+ *		break
+ *	
+ *	index1++
+ *
+ * return found_index
+ *
+ *
+ */	
