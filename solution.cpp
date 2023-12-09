a7f849620bdd04d3297430190e9e3d06714c0ce4
Kyle Nguyen <knguyen1193@outlook.com>
2023-12-08 23:35:45 -0800

    still fixing

--- solution.cpp
+++ solution.cpp
@@ -1,1 +1,15 @@
 #include "iostream"
+#include <vector>
+using namespace std;
+
+int maxArea(vector<int>& height) {
+        
+    }
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
