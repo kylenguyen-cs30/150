9719a805d67edf6675d486c26de471b1cd35a08b
Kyle <knguyen1193@outlook.com>
2023-12-11 18:08:21 -0800

    add files

--- solution.cpp
+++ solution.cpp
@@ -1,77 +1,28 @@
 #include "iostream"
 #include "vector"
-#include "numeric"
+#include "string"
+
 using namespace std;
 
 
-int minSubArrayLen(int target, vector<int>& nums){
-    int min_len = nums.size() + 1;
-    int sum = 0;
-    int left = 0;
-    int right = 0;
-
-
-    while(right < nums.size()){
-        sum += nums[right];
-        while (sum >= target)
-        {
-            min_len = min(min_len, right - left + 1);
-            sum -= nums[left];
-            left++;
-        }
-        right++;
-    }
-
-    if(min_len == nums.size()+1){
-        return 0;
-    }else{
-        return min_len;
-    }
+int lengthOfLongestSubstring(string s){
+    string temp = s;
+    sort(temp.begin(), temp.end());
+    cout<< temp << endl;
+    
+    return 0;
 
 }
-/*
-
-*/
-
 
 int main(int argc, char const *argv[])
 {
-    // input
-    vector<int>nums = {1,4,4};
-    int target = 4;
-    int result = minSubArrayLen(target, nums);
+    string s = "abcabcbcbb";
+    int result = lengthOfLongestSubstring(s);
     cout << result << endl;
+    
     return 0;
 }
 
 /*
-- an array of positve integers
-- a positive integer target
-- return the minimal length of a subarray
-- sum of the sub array is greater or equal target
-- if there is no subarray. return 0
-
-pseudocode:
-
-int target
-vector<int>nums
-
-base case 
-int sum = 0
-accumulate(nums.begin(),nums.end(), sum)
-
-if sum < target:
-    return 0
-
-main case
-sort(v.begin(), v.end())
-
-int left = 0
-int right = 0 
-
-while(right != nums.size()){
-
-}
-
 
 */
